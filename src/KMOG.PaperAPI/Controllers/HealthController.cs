43f49ca7c1d829bdba42637eda762eda05479eb1
DMPatod <danielmenator@gmail.com>
2021-05-27 22:49:47 -0300

    HealthControler added

---
+++ src/KMOG.PaperAPI/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading.Tasks;
+
+namespace KMOG.PaperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok("ok");
+        }
+    }
+}
